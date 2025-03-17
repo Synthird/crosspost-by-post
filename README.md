5779a318c6088c55224adecbf624249aff28d2be
Synthird <105394020+Synthird@users.noreply.github.com>
2025-03-16 23:26:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Crosspost by post
+
+Crosspost on one platform at a time.
+
+## Third-party links used
+
+- [Tootpick](https://github.com/Juerd/tootpick) for the posting to Mastodon functionality
+
+## License
+
---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const textArea = document.getElementById("text-area");
+
+const buttonDiv = document.getElementById("post-buttons");
+const buttons = buttonDiv.children;
+
+textArea.addEventListener("input", () => {
+	for (let elementIndex = 0; elementIndex < buttons.length; elementIndex++) {
+		let button = buttons[elementIndex];
+
+		if (button.tagName === "A") {
+			let buttonLink = button.getAttribute("href");
+			let prefix = buttonLink.substring(0, buttonLink.indexOf("="));
+			let fullURL = `${prefix}=${textArea.value}`;
+
+			button.setAttribute("href", fullURL);
+		}
+	}
+});
