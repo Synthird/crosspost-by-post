6f7de5db4a684e5eed39696c7562675633de2e87
Synthird <105394020+Synthird@users.noreply.github.com>
2025-03-20 21:30:18 -0700

    Added the ability to toggle light and dark mode

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
 const textArea = document.querySelector("textarea");
+const root = document.querySelector(":root");
 
 const buttons = document.getElementById("post-buttons").children;
 
+// Theme options
+const browserThemeOption = document.getElementById("browser-theme");
+const darkThemeOption = document.getElementById("dark-theme");
+const lightThemeOption = document.getElementById("light-theme");
+
 const connectorURL = "=";
 
+function setTheme(themeName) {
+	root.style.colorScheme = themeName;
+}
+
+// Typing in the textarea for changing the button links
 textArea.addEventListener("keyup", () => {
 	for (let elementIndex = 0; elementIndex < buttons.length; elementIndex++) {
 		const button = buttons[elementIndex];
@@ -19,4 +30,18 @@ textArea.addEventListener("keyup", () => {
 				break;
 		}
 	}
+});
+
+// Changing themes
+
+browserThemeOption.addEventListener("click", () => {
+	setTheme("light dark")
+});
+
+darkThemeOption.addEventListener("click", () => {
+	setTheme("dark");
+});
+
+lightThemeOption.addEventListener("click", () => {
+	setTheme("light");
 });
