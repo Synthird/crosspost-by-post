21e8e340e6448b66479fe69ce30e812e047279e2
Synthird <105394020+Synthird@users.noreply.github.com>
2025-03-20 17:40:12 -0700

    Replaced the textbox with a textarea to support multi-line text

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
-const textArea = document.querySelector("input[type='text']");
+const textArea = document.querySelector("textarea");
 
 const buttons = document.getElementById("post-buttons").children;
 
 const connectorURL = "=";
 
-textArea.addEventListener("input", () => {
+textArea.addEventListener("keyup", () => {
 	for (let elementIndex = 0; elementIndex < buttons.length; elementIndex++) {
 		const button = buttons[elementIndex];
 
 		switch (button.tagName) {
 			case "A":
 				const buttonLink = button.getAttribute("href");
+				const linkSubstring = buttonLink.slice(0, buttonLink.indexOf(connectorURL));
 
-				button.setAttribute("href", `${buttonLink.substring(0, buttonLink.indexOf(connectorURL))}${connectorURL}${textArea.value}`);
+				const text = encodeURIComponent(textArea.value);
+
+				button.setAttribute("href", `${linkSubstring}${connectorURL}${text}`);
 				break;
 		}
 	}
