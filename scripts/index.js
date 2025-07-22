19f7bebcbf0f0dc4b3bd6986bbd250a063e82003
Synthird <105394020+Synthird@users.noreply.github.com>
2025-07-21 18:38:58 -0700

    Removed the switch statement as the buttons array only contains a tags

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,12 @@ const textArea = document.querySelector("textarea"),
 
 textArea.addEventListener("input", () => {
 	for (const button of buttons) {
-		switch (button.tagName) {
-			case "A":
-				// Link pieces
-				const buttonLink = button.getAttribute("href"),
-					beginningLink = buttonLink.slice(0, buttonLink.indexOf("=") + 1),
-					text = encodeURIComponent(textArea.value);
+		// Link pieces
+		const buttonLink = button.getAttribute("href"),
+			beginningLink = buttonLink.slice(0, buttonLink.indexOf("=") + 1),
+			text = encodeURIComponent(textArea.value);
 
-				// Change links
-				button.setAttribute("href", `${beginningLink}${text}`);
-				break;
-		}
+		// Change links
+		button.setAttribute("href", `${beginningLink}${text}`);
 	}
 });
